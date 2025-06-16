47c6594f9ce2592fcdbe5217dc9560453ecf18d8
Polar0706 <a20222238@pucp.edu.pe>
2025-06-16 10:27:29 -0500

    lab 8

--- Lab8_20222238_GTICS/src/main/java/com/tele/lab8_20222238_gtics/entity/MonitoreoClimatico.java
+++ Lab8_20222238_GTICS/src/main/java/com/tele/lab8_20222238_gtics/entity/MonitoreoClimatico.java
@@ -1,1 +1,30 @@
 package com.tele.lab8_20222238_gtics.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "monitoreo_climatico")
+@Data
+public class MonitoreoClimatico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String ciudad;
+
+    private LocalDate fecha;
+
+    @Column(name = "temp_promedio")
+    private Double tempPromedio;
+
+    @Column(name = "condicion_frecuente")
+    private String condicionFrecuente;
+
+    @Column(name = "temp_max")
+    private Double tempMax;
+
+    @Column(name = "temp_min")
+    private Double tempMin;
+}
--- Lab8_20222238_GTICS/src/main/java/com/tele/lab8_20222238_gtics/repository/MonitoreoClimaticoRepository.java
+++ Lab8_20222238_GTICS/src/main/java/com/tele/lab8_20222238_gtics/repository/MonitoreoClimaticoRepository.java
@@ -1,1 +1,9 @@
 package com.tele.lab8_20222238_gtics.repository;
+
+import tele.lab8_20222238_gtics.entity.MonitoreoClimatico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MonitoreoClimaticoRepository extends JpaRepository<MonitoreoClimatico, Integer> {
+}
